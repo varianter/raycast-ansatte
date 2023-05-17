0e31ddcc54ba291652144b104b46a1156809487d
Adrian Santana Berg <adrian_berg96@hotmail.com>
2023-05-18 01:32:14 +0200

    feat: Add a detailed view for employee
    Also changed Prettier's `printWidth` just because.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import { useEffect } from "react";
-import { ActionPanel, Action, Grid, showToast, Toast, Clipboard } from "@raycast/api";
+import {
+  ActionPanel,
+  Action,
+  Grid,
+  showToast,
+  Toast,
+  Clipboard,
+  Detail,
+  Icon,
+} from "@raycast/api";
 import { useCachedState, useFetch } from "@raycast/utils";
 import downloadTempImage from "./utils/downloadTempImage";
 
@@ -9,6 +18,7 @@ type EmployeeItem = {
   telephone: string | null;
   imageUrl: string;
   officeName: string;
+  startDate: string;
 };
 
 const BASE_URL = "https://chewie-webapp-ld2ijhpvmb34c.azurewebsites.net";
@@ -23,7 +33,26 @@ export default function Command() {
   const [columns, setColumns] = useCachedState<Columns>("columns", 5);
   const [office, setOffice] = useCachedState<Office>("office", "Alle");
 
-  const { data, error, isLoading } = useFetch<{ employees: EmployeeItem[] }>(`${BASE_URL}/employees`);
+  const [employees, setEmployees] = useCachedState<EmployeeItem[]>(
+    "employees",
+    []
+  );
+
+  const { data, error, isLoading } = useFetch<{ employees: EmployeeItem[] }>(
+    `${BASE_URL}/employees`
+  );
+
+  useEffect(() => {
+    if (data) {
+      setEmployees(
+        data.employees
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .filter((employee) =>
+            office === "Alle" ? true : employee.officeName === office
+          )
+      );
+    }
+  }, [data, office]);
 
   useEffect(() => {
     if (error) {
@@ -40,6 +69,8 @@ export default function Command() {
       columns={columns}
       inset={Grid.Inset.Large}
       isLoading={isLoading}
+      navigationTitle={`Viser ${employees.length} ansatte`}
+      searchBarPlaceholder="Søk etter ansatt"
       searchBarAccessory={
         <Grid.Dropdown
           tooltip="Velg kontor"
@@ -53,54 +84,146 @@ export default function Command() {
         </Grid.Dropdown>
       }
     >
-      {!isLoading &&
-        data?.employees
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .filter((employee) => (office === "Alle" ? true : employee.officeName === office))
-          .map((employee) => (
-            <Grid.Item
-              key={employee.name}
-              content={{ value: { source: employee.imageUrl }, tooltip: employee.name }}
-              title={employee.name}
-              keywords={[employee.officeName, employee.email]}
-              subtitle={employee.officeName}
-              actions={
-                <ActionPanel>
+      {employees.length ? (
+        employees.map((employee) => (
+          <Grid.Item
+            key={employee.name}
+            content={{
+              value: { source: employee.imageUrl },
+              tooltip: employee.name,
+            }}
+            title={employee.name}
+            keywords={[employee.officeName, employee.email]}
+            subtitle={employee.officeName}
+            actions={
+              <ActionPanel>
+                <Action.Push
+                  target={<Employee employee={employee} />}
+                  title="Se info"
+                  shortcut={{ modifiers: ["cmd"], key: "i" }}
+                  icon={Icon.MagnifyingGlass}
+                />
+                <Action.CopyToClipboard
+                  content={employee.email}
+                  title="Kopier e-post"
+                  shortcut={{ modifiers: ["cmd"], key: "e" }}
+                />
+                <Action.CopyToClipboard
+                  content={employee.name}
+                  title="Kopier navn"
+                  shortcut={{ modifiers: ["cmd"], key: "n" }}
+                />
+                {employee.telephone !== null && (
                   <Action.CopyToClipboard
-                    content={employee.email}
-                    title="Kopier e-post"
-                    shortcut={{ modifiers: ["cmd"], key: "e" }}
+                    content={employee.telephone}
+                    title="Kopier telefonnummer"
+                    shortcut={{ modifiers: ["cmd"], key: "t" }}
                   />
-                  <Action.CopyToClipboard
-                    content={employee.name}
-                    title="Kopier navn"
-                    shortcut={{ modifiers: ["cmd"], key: "n" }}
-                  />
-                  {employee.telephone != null && (
-                    <Action.CopyToClipboard
-                      content={employee.telephone || ""}
-                      title="Kopier telefonnummer"
-                      shortcut={{ modifiers: ["cmd"], key: "t" }}
+                )}
+                <Action.CopyToClipboard
+                  content={employee.imageUrl}
+                  title="Kopier bilde"
+                  shortcut={{ modifiers: ["cmd"], key: "b" }}
+                  onCopy={async (content) => {
+                    const image = await downloadTempImage(
+                      content.toString(),
+                      employee.name
+                    );
+                    Clipboard.copy({ file: image });
+                  }}
+                />
+                <ActionPanel.Submenu
+                  title="Sett antall kolonner"
+                  icon={Icon.AppWindowGrid3x3}
+                >
+                  {columnChoices.map((choice) => (
+                    <Action
+                      key={choice}
+                      title={choice.toString()}
+                      onAction={() => setColumns(choice)}
                     />
-                  )}
-                  <Action.CopyToClipboard
-                    content={employee.imageUrl}
-                    title="Kopier bilde"
-                    shortcut={{ modifiers: ["cmd"], key: "b" }}
-                    onCopy={async (content) => {
-                      const image = await downloadTempImage(content.toString(), employee.name);
-                      Clipboard.copy({ file: image });
-                    }}
-                  />
-                  <ActionPanel.Submenu title="Sett antall kolonner">
-                    {columnChoices.map((choice) => (
-                      <Action key={choice} title={choice.toString()} onAction={() => setColumns(choice)} />
-                    ))}
-                  </ActionPanel.Submenu>
-                </ActionPanel>
-              }
-            />
-          ))}
+                  ))}
+                </ActionPanel.Submenu>
+              </ActionPanel>
+            }
+          />
+        ))
+      ) : (
+        <Grid.EmptyView title="Kunne ikke hente ansatte" />
+      )}
     </Grid>
+  );
+}
+
+function Employee({ employee }: { employee: EmployeeItem }) {
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const flag = employee.officeName === "Stockholm" ? "🇸🇪" : "🇳🇴";
+
+  return (
+    <Detail
+      markdown={`<img src="${employee.imageUrl}" alt="" height="350">`}
+      navigationTitle={employee.name}
+      metadata={
+        <Detail.Metadata>
+          <Detail.Metadata.Label title="Navn" text={employee.name} />
+          <Detail.Metadata.Label title="E-post" text={employee.email} />
+          {employee.telephone !== null && (
+            <Detail.Metadata.Label
+              title="Telefonnummer"
+              text={employee.telephone}
+            />
+          )}
+          <Detail.Metadata.Label
+            title="Startdato"
+            text={new Date(employee.startDate).toLocaleDateString(
+              "nb-NO",
+              dateOptions
+            )}
+          />
+          <Detail.Metadata.Label
+            title="Kontor"
+            text={`${flag} ${employee.officeName}`}
+          />
+        </Detail.Metadata>
+      }
+      actions={
+        <ActionPanel>
+          <Action.CopyToClipboard
+            content={employee.email}
+            title="Kopier e-post"
+            shortcut={{ modifiers: ["cmd"], key: "e" }}
+          />
+          <Action.CopyToClipboard
+            content={employee.name}
+            title="Kopier navn"
+            shortcut={{ modifiers: ["cmd"], key: "n" }}
+          />
+          {employee.telephone != null && (
+            <Action.CopyToClipboard
+              content={employee.telephone || ""}
+              title="Kopier telefonnummer"
+              shortcut={{ modifiers: ["cmd"], key: "t" }}
+            />
+          )}
+          <Action.CopyToClipboard
+            content={employee.imageUrl}
+            title="Kopier bilde"
+            shortcut={{ modifiers: ["cmd"], key: "b" }}
+            onCopy={async (content) => {
+              const image = await downloadTempImage(
+                content.toString(),
+                employee.name
+              );
+              Clipboard.copy({ file: image });
+            }}
+          />
+        </ActionPanel>
+      }
+    />
   );
 }
